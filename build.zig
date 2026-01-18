87355c9f2f7c1c3c77520f2b694ece214ed99315
StuffedRat46810 <alon@moranoa.com>
2026-01-18 13:17:00 +0200

    added tests

--- build.zig
+++ build.zig
@@ -22,7 +22,6 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/config_utils.zig"),
         .link_libc = true,
     });
-
     const exe = b.addExecutable(.{
         .name = "albumfetch",
         .root_module = b.createModule(.{
@@ -43,71 +42,34 @@ pub fn build(b: *std.Build) void {
     }
     b.installArtifact(exe);
 
-    // This creates a top level step. Top level steps have a name and can be
-    // invoked by name when running `zig build` (e.g. `zig build run`).
-    // This will evaluate the `run` step rather than the default step.
-    // For a top level step to actually do something, it must depend on other
-    // steps (e.g. a Run step, as we will see in a moment).
     const run_step = b.step("run", "Run the app");
 
-    // This creates a RunArtifact step in the build graph. A RunArtifact step
-    // invokes an executable compiled by Zig. Steps will only be executed by the
-    // runner if invoked directly by the user (in the case of top level steps)
-    // or if another step depends on it, so it's up to you to define when and
-    // how this Run step will be executed. In our case we want to run it when
-    // the user runs `zig build run`, so we create a dependency link.
     const run_cmd = b.addRunArtifact(exe);
     run_step.dependOn(&run_cmd.step);
 
-    // By making the run step depend on the default step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
     run_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    // Creates an executable that will run `test` blocks from the provided module.
-    // Here `mod` needs to define a target, which is why earlier we made sure to
-    // set the releative field.
-    // const mod_tests = b.addTest(.{
-    //     .root_module = b.path(""),
-    // });
-    //
-    // // A run step that will run the test executable.
-    // const run_mod_tests = b.addRunArtifact(mod_tests);
-
-    // Creates an executable that will run `test` blocks from the executable's
-    // root module. Note that test executables only test one module at a time,
-    // hence why we have to create two separate ones.
     const exe_tests = b.addTest(.{
         .root_module = b.createModule(.{
-            .root_source_file = b.path("tests/tests.zig"),
+            .root_source_file = b.path("src/tests.zig"),
             .target = target,
             .optimize = optimize,
             .link_libc = true,
         }),
     });
 
-    exe_tests.root_module.addImport("album_utils", album_utils_mod);
-    exe_tests.root_module.addImport("config_utils", config_utils_mod);
+    // exe_tests.root_module.addImport("album_utils", album_utils_mod);
+    // exe_tests.root_module.addImport("config_utils", config_utils_mod);
+    // exe_tests.root_module.addImport("color_utils", color_utils_mod);
+    // exe_tests.root_module.addImport("album", album_mod);
+    // exe_tests.root_module.addImport("time_utils", time_mod);
     const run_exe_tests = b.addRunArtifact(exe_tests);
 
     const test_step = b.step("test", "Run tests");
     // test_step.dependOn(&run_mod_tests.step);
     test_step.dependOn(&run_exe_tests.step);
-
-    // Just like flags, top level steps are also listed in the `--help` menu.
-    //
-    // The Zig build system is entirely implemented in userland, which means
-    // that it cannot hook into private compiler APIs. All compilation work
-    // orchestrated by the build system will result in other Zig compiler
-    // subcommands being invoked with the right flags defined. You can observe
-    // these invocations when one fails (or you pass a flag to increase
-    // verbosity) to validate assumptions and diagnose problems.
-    //
-    // Lastly, the Zig build system is relatively simple and self-contained,
-    // and reading its source code will allow you to master it.
 }
